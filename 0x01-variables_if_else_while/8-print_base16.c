5e2b13c57390c4da90e9dd0bf51350013933b27f
Hanan <se.hanan.miftah@gmail.com>
2022-03-12 01:16:40 +0300

    eski demo

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,12 +9,12 @@
 
 int main(void)
 {
-	int i, y;   
+int i, y;   
 for (i = 0; i < 10; i++)
 	{
 		putchar (i + '0');
 	}
-	for (y = 'a'; y <= 'f'; y++)
+for (y = 'a'; y <= 'f'; y++)
 	{
 		putchar (y);
 	}
